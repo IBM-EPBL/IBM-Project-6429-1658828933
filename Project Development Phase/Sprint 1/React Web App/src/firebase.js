e79ef9a58b2bed800d252fbf326f481e29b3701c
Hari Prasath <68268300+Hari-prasath855@users.noreply.github.com>
2022-11-08 19:57:45 +0530

    Sprint 1

---
+++ Project Development Phase/Sprint 1/React Web App/src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcOcMZ7bTgSSlBHThr6DQ8wjAEQKbHqrI",
+  authDomain: "fire-management-system-19220.firebaseapp.com",
+  projectId: "fire-management-system-19220",
+  storageBucket: "fire-management-system-19220.appspot.com",
+  messagingSenderId: "471571648481",
+  appId: "1:471571648481:web:811b80171aaa5bb2578ca0"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+export {firebase, firebaseApp};
